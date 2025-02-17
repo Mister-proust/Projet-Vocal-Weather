1a97548a32ca7e12e9a290c7cb5b879d4e982c51
Mister-proust <maximilien_proust@hotmail.fr>
2025-02-17 14:54:07 +0100

    fix Speech_recognition.py, MAJ readme and requirements

--- README.md
+++ README.md
@@ -24,3 +24,20 @@ env\Scripts\activate
 ```bash
 pip install -r requirements.txt
 ```
+
+4. Connexion à la base de données et fichier caché
+
+Les paramètres de connexion à la base de données ainsi que les données sensibles doivent être mises dans un fichier ```.env```.
+
+```
+# fichier .env
+
+DB_HOST="database_host"
+DB_PORT="server_port"
+DB_USER="username"
+DB_PASS="password"
+DB_NAME="database_name"
+
+SPEECH_KEY = "YourKey"
+SPEECH_REGION = "YourRegion"
+```
--- Scripts/speech_recognition.py
+++ Scripts/speech_recognition.py
@@ -1,11 +1,15 @@
 import os
 import azure.cognitiveservices.speech as speechsdk
+from dotenv import load_dotenv
 
+load_dotenv()
 
+speech_key = os.getenv("SPEECH_KEY")
+service_region = os.getenv("SPEECH_REGION")
 
 def recognize_from_microphone():
     # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('54124b94ae904eeea1d8a652a4c3d88d'), region=os.environ.get('francecentral'))
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
     speech_config.speech_recognition_language="fr-FR"
 
     audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
